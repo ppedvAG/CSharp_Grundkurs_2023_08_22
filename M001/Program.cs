df962c987ef00447c5145bfdc95484754dbe7222
ppedvLukasK <lukask@ppedv.de>
2023-08-22 16:19:33 +0200

    M001

--- M001/Program.cs
+++ M001/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Strg + .: Schnellaktionen unter dem Cursor anzeigen
+//Strg + Leertaste: Vorschläge öffnen
+//Strg + K, Strg + D: Code formatieren
+//Strg + K, Strg + C: Codeblock auskommentieren
+//Strg + K, Strg + U: Codeblock einkommentieren
+//Alt + Pfeiltaste: Derzeitige Zeile verschieben
+//Strg + R + R: Feld umbenennen, wird überall umbenannt
