4c9123d18c3af60c820311f901dd3c01afd61aea
ppedvLukasK <lukask@ppedv.de>
2023-08-23 10:33:29 +0200

    M006

---
+++ M006/Kurs.cs
@@ -0,0 +1,26 @@
+﻿namespace M006.Data
+{
+	internal class Kurs
+	{
+		public Person[] Teilnehmer;
+
+		public Standort Ort;
+
+		public Kurs(Standort ort, params Person[] tn)
+		{
+			Ort = ort;
+			Teilnehmer = new Person[tn.Length];
+			for (int i = 0; i < tn.Length; i++)
+			{
+				Teilnehmer[i] = tn[i];
+			}
+		}
+	}
+
+	public enum Standort
+	{
+		Präsenz,
+		Virtuell,
+		Gemischt
+	}
+}
