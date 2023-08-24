1a35afd2e958dc4a539bd693c583294d3cd31d70
ppedvLukasK <lukask@ppedv.de>
2023-08-24 16:42:11 +0200

    M012

--- M001/Program.cs
+++ M001/Program.cs
@@ -10,3 +10,34 @@ Console.WriteLine("Hello, World!");
 //Strg + R + R: Feld umbenennen, wird überall umbenannt
 //F12: Zu dem Member unter dem Cursor springen
 //Strg + D: Derzeitige Zeile kopieren
+
+new Queue<Fahrzeug>(new Fahrzeug[10].Select(e => Fahrzeug.GenFahrzeug())
+	.Zip(new Stack<Fahrzeug>(new Fahrzeug[10].Select(e => Fahrzeug.GenFahrzeug())
+	.Where(e => e is IBeladbar)
+	.ToList()
+	.ToDictionary(e => e.First, e => e.Second);
+Console.WriteLine();
+
+public class Fahrzeug
+{
+	public static Fahrzeug GenFahrzeug() => Random.Shared.Next(0, 3) switch { 1 => new PKW(), 2 => new Schiff(), _ => new Flugzeug() };
+}
+
+public class PKW : Fahrzeug { }
+
+public class Schiff : Fahrzeug, IBeladbar
+{
+	public Fahrzeug Ladung { get; set; }
+
+	public void Belade(Fahrzeug f)
+	{
+		Ladung = f;
+	}
+}
+
+public class Flugzeug : Fahrzeug { }
+
+public interface IBeladbar 
+{
+	void Belade(Fahrzeug f);
+}
